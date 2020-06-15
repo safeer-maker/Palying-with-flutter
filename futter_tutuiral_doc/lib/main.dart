d2264ad5427566fd2488340af4adec6198c06419
Safeer_linux <bsee15055@itu.edu.pk>
2020-06-15 13:34:03 +0500

    tutorial 7 completed

--- futter_tutuiral_doc/lib/main.dart
+++ futter_tutuiral_doc/lib/main.dart
@@ -5,26 +5,40 @@ void main() {
     //Widget build(BuildContext context){
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("My first app"),
-
-          backgroundColor: Colors.amber,
-          //  child: Container(
-          //  color: Colors.cyanAccent,
-          //),
-        ),
-        body: Container(
-          color: Colors.blueGrey,
-        ),
-        bottomNavigationBar: BottomAppBar(
-          child: Container(
-            color: Colors.blue,
-            height: 50.0,
-          ),
-        ),
-      ),
-      // };
+      home: home(),
     ),
   );
 }
+
+
+class home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My first app"),
+        backgroundColor: Colors.amber,
+        //  child: Container(
+        //  color: Colors.cyanAccent,
+        //),
+      ),
+      body: Container(
+        color: Colors.blueGrey,
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          color: Colors.indigo,
+          height: 50.0,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blue,
+        child: Text(
+          "Button",
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
+
